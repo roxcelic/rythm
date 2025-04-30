aa6084e81a2ca458f30cba5d6149f60dabfa6d81
mushy <157179472+roxcelic@users.noreply.github.com>
2025-04-30 22:09:11 +0100

    chat update

--- src/res/js/hold/chat.js
+++ src/res/js/hold/chat.js
@@ -1,27 +1,81 @@
 function spawnMessage(message){
-    let newMessage = document.createElement("P");
-    newMessage.textContent = message[0];
-    newMessage.style.color = message[1];
-
-    return newMessage;
+    try {
+        const formattedDate = new Intl.DateTimeFormat('en-GB', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric'
+        }).format(new Date(message[4]));
+    
+        let pappaMessage = document.createElement("P");
+        pappaMessage.style.fontSize = "xx-small";
+        pappaMessage.style.marginTop = "5px";
+        pappaMessage.style.marginLeft = "5px";
+        pappaMessage.textContent = `${message[3]}--${formattedDate}--${new Date(message[4]).toTimeString().split(' ')[0]}`;
+        pappaMessage.id = message[2];
+    
+        let newMessage = document.createElement("P");
+        newMessage.style.fontSize = "medium";
+        newMessage.style.marginLeft = "-5px";
+        newMessage.textContent = message[0];
+        newMessage.style.color = message[1];
+    
+        window.scrollTo(0, document.body.scrollHeight);
+    
+        pappaMessage.appendChild(newMessage);
+        return pappaMessage;
+    } catch (e) {
+        return document.createElement("P");
+    }
 }
 
 function getOldChat(chat){
-    let fullchatlog = chat.childNodes.length;
+    chat = [...chat.children];
+
+    let fullchatlog = chat.map((message) => [message.textContent, message.style.color, message.id]);
+
     return fullchatlog;
 }
 
-async function displayChat(interval = null) {
-    let chat = document.getElementById("chat");
+export async function deleteChat() {
     let apiRoot = `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':3000' : ''}/`;
 
-    try {
-        let response = await fetch(`${apiRoot}api/v1/chat/view`)
-        let data = await response.json();
-        data.chat = Array.isArray(data.chat) ? data.chat : [["basic message", "#783432"]];
+    await fetch(`${apiRoot}api/v1/chat/delete?chatName=${document.getElementById("chatroom").value}`);
+}
 
-        let difference = data.chat.slice(getOldChat(chat));
-        
+async function displayChat(interval = null, chat = null, speed = null, delay = 0) {
+    let apiRoot = `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':3000' : ''}/`;
+
+    if (speed != null && interval != null && speed.value != delay){
+        console.log("changing chat refresh speed");
+        clearInterval(interval);
+        loadChat(speed.value);
+        console.log("changed chat value");
+        return null;
+    }
+
+    try {
+        let response = await fetch(`${apiRoot}api/v1/chat/view?chatName=${document.getElementById("chatroom").value}`);
+        let data = await response.json();
+        data.chat = Array.isArray(data.chat) ? data.chat : [["basic message", "#783432", 0]];
+
+        let previousMessages = getOldChat(chat);
+        let difference = [];
+        let index = 0;
+
+        if (previousMessages[0] == undefined){
+            difference = data.chat;
+        } else if (previousMessages[previousMessages.length - 1][2] != data.chat[data.chat.length - 1][2]){  
+            let finalSnow = previousMessages[previousMessages.length - 1][2];
+              
+            data.chat.forEach((message, ChatIndex) => {
+                if (message[2] == finalSnow) index = ChatIndex + 1;
+            });
+
+            difference = data.chat.slice(index);
+        } else {
+            console.log("unchaged");
+        }
+
         difference.forEach(message => {
             chat.appendChild(spawnMessage(message));
         });
@@ -33,21 +87,28 @@ async function displayChat(interval = null) {
     }
 }
 
-export async function loadChat() {
+export async function loadChat(delay = 0) {
+    let chat = document.getElementById("chat");
+    let speed = document.getElementById("chatSpeed");
+    delay = speed.value;
+
     let display = setInterval(() => {
-        displayChat(display, [])
-    }, 1000); 
+        displayChat(display, chat, speed, delay);
+        console.log("refreshed chat");
+    }, delay); 
 }
 
 export async function sendMessage() {
+    let apiRoot = `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':3000' : ''}/`;
+
     let data = {
         upload: document.getElementById("chatMessage").value,
-        color: document.getElementById("chatColor").value
+        color: document.getElementById("chatColor").value,
+        name: document.getElementById("chatname").value,
+        chatName: document.getElementById("chatroom").value
     }
 
     document.getElementById("chatMessage").value = "";
-
-    let apiRoot = `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':3000' : ''}/`;
 
     try {
         await fetch(`${apiRoot}api/v1/chat/post`, {
@@ -58,7 +119,8 @@ export async function sendMessage() {
             body: JSON.stringify(data)
         });
 
-        displayChat();
+        let chat = document.getElementById("chat");
+        displayChat(null, chat);
     } catch (e) {
         console.log(e);
     }
--- src/res/js/script.js
+++ src/res/js/script.js
@@ -2,7 +2,7 @@ import { loadContent, checkSecurity } from "./hold/popup";
 import { LoadPosts, submitPost, LoadPost, deletePost } from "./hold/blog";
 import { DisplayImage, submit, loadImages, submitAlt } from "./hold/image";
 import { postStatus } from "./hold/status";
-import { loadChat, sendMessage } from "./hold/chat";
+import { loadChat, sendMessage, deleteChat } from "./hold/chat";
 
 addEventListener("DOMContentLoaded", (event) => {
     let path = ((window.location.pathname).split("/"))[2];
@@ -52,11 +52,39 @@ addEventListener("DOMContentLoaded", (event) => {
 
             loadChat();
             document.getElementById("chatSend").addEventListener("click", sendMessage);
+            document.getElementById("chatDelete").addEventListener("click", deleteChat);
             
             document.getElementById("chatMessage").addEventListener('keydown', function(event) {
                 if (event.key === 'Enter') {
                     sendMessage()
                 }
+            });
+
+            // save the color
+            let color = document.getElementById("chatColor");
+            color.value = document.cookie.split('; ').find(row => row.startsWith('color='))?.split('=')[1];
+            
+            let speed = document.getElementById("chatSpeed");
+            let speedval = document.cookie.split('; ').find(row => row.startsWith('speed='))?.split('=')[1];
+            speed.value = parseInt(speedval != undefined ? parseInt(speedval) : 1000);
+
+            let chatRoom = document.getElementById("chatroom");
+            chatRoom.textContent = document.cookie.split('; ').find(row => row.startsWith('room='))?.split('=')[1];
+            
+            color.addEventListener('input', function() {
+                document.cookie = `color=${this.value}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
+            });
+            
+            speed.addEventListener('input', function() {
+                document.cookie = `speed=${this.value}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
+            });
+
+            chatRoom.addEventListener('input', function() {
+                document.cookie = `room=${this.value}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
+            });
+
+            chatRoom.addEventListener('input', function() {
+                document.getElementById("chat").innerHTML = "";
             });
 
             break;
