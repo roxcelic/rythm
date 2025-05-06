3e60b5b18c18c8f0ef991803880529b7cb7aa7d9
mushy <157179472+roxcelic@users.noreply.github.com>
2025-05-06 06:26:00 +0100

    revamp whatever

--- src/res/js/hold/popup.js
+++ src/res/js/hold/popup.js
@@ -72,25 +72,43 @@ export async function loadContent() {
 
 export async function checkSecurity() {
     let apiRoot = `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':3000' : ''}/api/v1/admin/viewlog`;
+    //apiRoot = 'https://api.roxcelic.love/api/v1/admin/viewlog';
 
     try {
         let resposne = await fetch(apiRoot );
         let data = await resposne.json();
         let ips = {
-            "succesfull": [],
-            "unsuccesfull": []
+            "base": {
+                "succesfull": [],
+                "unsuccesfull": []
+            },
+            "true": {
+                "succesfull": [],
+                "unsuccesfull": []
+            }
         };
 
         data.status.forEach(ip => {
-            if (ip.succefull && !ips.succesfull.includes(ip.ip)){
-                ips.succesfull.push(ip.ip);
-            } else if (!ip.succefull && !ips.unsuccesfull.includes(ip.ip)){
-                ips.unsuccesfull.push(ip.ip);
+            console.log(ip);
+            if (ip.access = "base"){
+                if (ip.succefull && !ips.base.succesfull.includes(ip.ip)){
+                    ips.base.succesfull.push(ip.ip);
+                } else if (!ip.succefull && !ips.base.unsuccesfull.includes(ip.ip)){
+                    ips.base.unsuccesfull.push(ip.ip);
+                }
+            }
+            if (ip.access = "True") {
+                if (ip.succefull && !ips.true.succesfull.includes(ip.ip)){
+                    ips.true.succesfull.push(ip.ip);
+                } else if (!ip.succefull && !ips.true.unsuccesfull.includes(ip.ip)){
+                    ips.true.unsuccesfull.push(ip.ip);
+                }
             }
         });
 
         popup(JSON.stringify(ips, null, 4));
     } catch (e) {
+        console.log(e);
         popup(e);
     }
 }
